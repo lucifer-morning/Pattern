1719277ad917a304d28e6cb7ca1f2711b4257ee1
lucifer-morning <32412012+lucifer-morning@users.noreply.github.com>
2020-02-17 16:58:44 +0530

    Add files via upload

---
+++ Assets/Scripts/ButtonManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ButtonManager : MonoBehaviour
+{
+    public Button btn;
+    public string s;
+    public delegate void MyDelegate(char num);
+    public static MyDelegate myDelegate;
+    public Coroutine game;
+    private IEnumerator Inum_Game_started()
+    {
+        btn.image.color = Color.red;
+        yield return new WaitForSeconds(1);
+        btn.image.color = Color.white;
+    }
+    private IEnumerator Inum_Game_started_Correct()
+    {
+        btn.image.color = Color.green;
+        yield return new WaitForSeconds(1);
+        btn.image.color = Color.white;
+    }
+    public void TaskOnClick()
+    {
+        Debug.Log("Button pressed");
+
+        myDelegate(s[0]);
+    }
+    public void generate_event(char c)
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        btn = this.gameObject.GetComponent<Button>();
+        s = this.gameObject.GetComponentInChildren<Text>().text;
+        btn.onClick.AddListener(TaskOnClick);
+        PatternManager.change_wrong_clr += change_clr_btn;
+        PatternManager.change_right_clr += change_clr_btn2;
+        //myDelegate(s[0]);
+    }
+
+    void change_clr_btn2(char c)
+    {
+        if (s[0] == c)
+        {
+            game = StartCoroutine(Inum_Game_started_Correct());
+        }
+        else
+        {
+            // game = StartCoroutine(Inum_Game_started_Correct());
+
+        }
+    }
+    void change_clr_btn(char c)
+    {
+        if(s[0]==c)
+        {
+            game = StartCoroutine(Inum_Game_started());
+        }
+        else
+        {
+           // game = StartCoroutine(Inum_Game_started_Correct());
+
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
---
+++ Assets/Scripts/PatternManager.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine;
+using System.Collections;
+
+public class PatternManager : MonoBehaviour
+{
+    public string pattern="7689";
+    public string[] color = { "<color='green'>", "</color>" };
+    public Text Pattern_Text;
+    public int digit_index = 0;
+
+    public Coroutine game;
+
+    public delegate void MyDelegate(char num);
+    public static MyDelegate change_wrong_clr;
+    public static MyDelegate change_right_clr;
+    public bool message_flag = false;
+    private IEnumerator Inum_Game_started_Correct()
+    {
+        Pattern_Text.text = "PATTERN MATCHED";
+        message_flag = true;
+        yield return new WaitForSeconds(2);
+        message_flag = false;
+        generate_new_pattern();
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        Pattern_Text = this.GetComponentInChildren<Text>();
+        update_pattern_ui();
+        ButtonManager.myDelegate += key_Pressed;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void key_Pressed(char digit)
+    {
+        Debug.Log("Key pressed");
+        if(digit== pattern[digit_index])
+        {
+            //change_clr(digit);
+            change_right_clr(digit);
+            digit_index++;
+            change_color(digit_index);
+            if(digit_index==4)
+            {
+                game = StartCoroutine(Inum_Game_started_Correct());
+                //while (message_flag == true) ;
+                //generate_new_pattern();
+            }
+        }
+        else
+        {
+            change_wrong_clr(digit);
+        }
+    }
+    void change_color(int index)
+    {
+        string new_str = pattern;
+        new_str=new_str.Insert(index, color[1]);
+        new_str = new_str.Insert(0, color[0]);
+        Pattern_Text.text = new_str;
+        //Debug.Log(new_str);
+    }
+    string generate_pattern()
+    {
+        string ret_str = "";
+        for(int i=0;i<4;i++)
+        {
+            
+            
+            //return getrandom.Next(min, max);
+            ret_str += UnityEngine.Random.Range(1,10).ToString();
+            
+            
+        }
+        return ret_str;
+    }
+    void generate_new_pattern()
+    {
+        pattern = generate_pattern();
+        digit_index = 0;
+        update_pattern_ui();
+    }
+    void update_pattern_ui()
+    {
+        Pattern_Text.text = pattern;
+    }
+}
